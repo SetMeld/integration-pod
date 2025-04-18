58f93dc35cccafe41a27c9e5e1c990775a1a53f3
Jackson Morgan <jaxoncreed@gmail.com>
2025-04-17 21:27:59 -0400

    install tailadmin

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,6 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "swiper/swiper-bundle.css";
+import "flatpickr/dist/flatpickr.css";
+import App from "./App.tsx";
+import { AppWrapper } from "./components/common/PageMeta.tsx";
+import { ThemeProvider } from "./context/ThemeContext.tsx";
 
-const App = () => <h1>Hello from UI</h1>;
-
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ThemeProvider>
+      <AppWrapper>
+        <App />
+      </AppWrapper>
+    </ThemeProvider>
+  </StrictMode>,
+);
